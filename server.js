242a9df307dd7112ef4813f6a3afc7eb51070ce0
sunnyradha <117989291+sunnyradha@users.noreply.github.com>
2026-03-23 02:29:57 +0530

    Set server to listen on specified port

--- server.js
+++ server.js
@@ -95,6 +95,7 @@ app.use((err, req, res, next) => {
 
   return next();
 });
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
