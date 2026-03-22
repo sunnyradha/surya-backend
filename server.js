0ea977d9c8930ad0426cabbf531f13b8bb6083b1
sunnyradha <117989291+sunnyradha@users.noreply.github.com>
2026-03-23 02:11:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -96,8 +96,6 @@ app.use((err, req, res, next) => {
   return next();
 });
 
-const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
